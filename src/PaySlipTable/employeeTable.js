8d78ff87e846b343e2b8aef2c08ddc95daff2753
Tushar Shyoran <shyoran.tushar@gmail.com>
2018-07-19 23:19:38 +0530

    Update employeeTable.js

--- src/PaySlipTable/employeeTable.js
+++ src/PaySlipTable/employeeTable.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import './employeeTable.css';
 import { NavLink } from 'react-router-dom';
 
 
 const employeeTable = (props) => {
-    return (
-        <div>
-        <BootstrapTable data={props.data}>
-          <TableHeaderColumn isKey dataField='name'>
-            Name
+  return (
+    <div>
+      <BootstrapTable data={props.data}>
+        <TableHeaderColumn isKey dataField='name'>
+          Name
           </TableHeaderColumn>
-          <TableHeaderColumn dataField='payperiod'>
-            Pay-Period
+        <TableHeaderColumn dataField='payperiod'>
+          Pay-Period
           </TableHeaderColumn>
-          <TableHeaderColumn dataField='grossIncome'>
-            Gross-income
+        <TableHeaderColumn dataField='grossIncome'>
+          Gross-income
           </TableHeaderColumn>
-          <TableHeaderColumn dataField='incomeTax'>
-            Income-tax
+        <TableHeaderColumn dataField='incomeTax'>
+          Income-tax
           </TableHeaderColumn>
-          <TableHeaderColumn dataField='netIncome'>
-            Net-income
+        <TableHeaderColumn dataField='netIncome'>
+          Net-income
           </TableHeaderColumn>
-          <TableHeaderColumn dataField='superAmount'>
-            Super-amount
+        <TableHeaderColumn dataField='superAmount'>
+          Super-amount
           </TableHeaderColumn>
-        </BootstrapTable>
+      </BootstrapTable>
 
-            <NavLink to="/" activeStyle={{
-                        textDecoration: 'none',
-                        fontWeight: 'bold',
-                        color: 'brown',
-                        padding: '80px',
-                        margin: '100px'
-            }}>Back </NavLink>
-
-      </div>
-
-    );
+      <NavLink to="/" activeStyle={{
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        color: 'brown',
+        padding: '80px',
+        margin: '100px'
+      }}>Back </NavLink>
+    </div>
+  );
 }
 
 export default employeeTable;
